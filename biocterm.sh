fe2574767f32fcc51a69cbc5fb6cbb084d337af0
lisancao <lisanatashacao@gmail.com>
2019-10-19 13:44:06 -0700

    more plugins

--- biocterm.sh
+++ biocterm.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env/bash
 
+####################################################
+
+#FILE         : biocterm.tmux
+#DESCRIPTION  : [WIP] This is a tmux plugin that is meant to be used to configure BiocTerm
+
+#OPTIONS      : ---
+#REQUIREMENTS : tmux
+#BUGS         : ---
+#NOTES        : ***THIS IS A PLACEHOLDER SCRIPT***, this script is currently used to directly launch certain shell scripts of different tmux plugins instead of having keybinds. Meant to be discrete with BiocTerm instance
+
+#AUTHOR       : LNC
+#CONTACT      : ---
+#DATE CREATED : Oct 18, 2019
+#LAST REVISION: --- 
+
+####################################################
+
+
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
  run-shell "$CURRENT_DIR/scripts/tmux_list_plugins.sh"
 
+##----------------------------tmux sensible
+#autoloads in .tmux.config
 
 ##----------------------------tmux logging 
 
@@ -13,4 +33,7 @@ source "$CURRENT_DIR/scripts/shared.sh"
 
 #save complete history
 run-shell "$CURRENT_DIR/scripts/save_complete_history.sh"
+
+
+##----------------------------tmux-resurrect + continuum
 
